cf08b448c2274e4effea7c286f1db9d57833050d
kamrulislam2 <kamrulislamABK@gmail.com>
2023-06-08 01:01:46 +0600

    Error page added

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Lottie from "lottie-react";
+import image from "../../assets/404-error.json";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>error</div>;
+  return (
+    <div className="relative">
+      <div className="absolute left-[703px] bottom-16 z-10">
+        <Link
+          to="/"
+          className="bg-[#FDD8D6] hover:bg-[#DDDCDC] cursor-pointer transition-transform px-5 py-3 text-xl font-medium "
+        >
+          Go Back
+        </Link>
+      </div>
+      <Lottie className="w-full h-[100vh]" animationData={image} loop={true} />
+    </div>
+  );
 };
 
 export default Error;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home";
+import Error from "../Pages/Error/Error";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    errorElement: <Error />,
     element: <Main></Main>,
     children: [
       {
